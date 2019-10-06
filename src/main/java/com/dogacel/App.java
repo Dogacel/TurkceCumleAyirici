c174e390815a4210ef361ce9b82fc43cd2595f91
Doğaç Eldenk <dogacel@gmail.com>
2019-10-07 01:55:58 +0300

    Revert "Shouldn't Merge (#2)"
    This reverts commit 93ce7953381dcd4fbb14d5132c037cf4ee9a1f16.

--- src/main/java/com/dogacel/App.java
+++ src/main/java/com/dogacel/App.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class Approke {
+public class App {
 
     static String[] words;
     static int wordCount, longestWord;

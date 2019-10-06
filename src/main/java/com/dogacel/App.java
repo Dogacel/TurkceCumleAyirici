fca6ad58f116e5ae75d738ef928cfb0e08f5b3db
Dogacel <dogacel@gmail.com>
2019-10-07 01:53:33 +0300

    Shouldn't Merge

--- src/main/java/com/dogacel/App.java
+++ src/main/java/com/dogacel/App.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class App {
+public class Approke {
 
     static String[] words;
     static int wordCount, longestWord;
